e845ba9fd8aa475e6e5cb4a626f35ad92ef2eefe
denizsimsek <denizsimsek@magnetcb.com>
2018-10-09 13:06:38 +0300

    first commit

---
+++ psd_to_html/HTML/chefGrup/js/main.js
@@ -0,0 +1,75 @@
+/* var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var cwidth = c.width;
+var cheight = c.height;
+
+ctx.moveTo(cwidth,0);
+ctx.lineTo(0, cheight);
+ctx.stroke(); */
+
+function onMenuIconClicked() {
+    console.log("Menu bar clicked.");
+}
+
+// Change site language
+var changeToEnglish = document.getElementById('english');
+changeToEnglish.addEventListener('click', function() {
+    document.body.className = 'en';
+});
+
+var changeToTurkish = document.getElementById('turkish');
+changeToTurkish.addEventListener('click', function() {
+    document.body.className = 'tr';
+});
+
+// Navigation
+function displayNoneAllSections() {
+    var sections = document.getElementsByTagName('section');
+    for(var i = 0; i < sections.length; i++)
+        sections[i].style.display = 'none';
+}
+
+function clearActiveStyle() {
+    var activeStyledHeader = document.getElementsByClassName('active');
+    if(activeStyledHeader.length > 0)
+        activeStyledHeader[0].className = activeStyledHeader[0].className.replace(" active", "");
+}
+
+var homeLink = document.getElementById('home-link');
+var homeSection = document.getElementById('home');
+homeLink.addEventListener('click', function(){
+    displayNoneAllSections();
+    homeSection.style.display = "block";
+
+    clearActiveStyle();
+});
+
+var aboutLink = document.getElementById('about-link');
+var aboutSection = document.getElementById('about');
+aboutLink.addEventListener('click', function(){
+    displayNoneAllSections();
+    aboutSection.style.display = "block";
+
+    clearActiveStyle();
+    this.className += " active";
+});
+
+var brandsLink = document.getElementById('brands-link');
+var brandsSection = document.getElementById('brands');
+brandsLink.addEventListener('click', function(){
+    displayNoneAllSections();
+    brandsSection.style.display = "block";
+
+    clearActiveStyle();
+    this.className += " active";
+});
+
+var contactLink = document.getElementById('contact-link');
+var contactSection = document.getElementById('contact');
+contactLink.addEventListener('click', function(){
+    displayNoneAllSections();
+    contactSection.style.display = "block";
+
+    clearActiveStyle();
+    this.className += " active";
+});
