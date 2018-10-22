2fb020825feb9ab9db9b04646f3b95eae2f079c5
Magnet Pro <macbookpro@Magnet-MacBook-Pro.local>
2018-10-22 16:43:27 +0300

    Delete console logs

--- HTML/chef-grup/js/main.js
+++ HTML/chef-grup/js/main.js
@@ -14,9 +14,9 @@ if('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('service_worker.js')
         .then(function(registration) { 
-            console.log("Service Worker Registered", registration); 
+            
         })
         .catch(function(err) {
-            console.log("Service Worker Failed to Register", err);
+            
         });
 }
