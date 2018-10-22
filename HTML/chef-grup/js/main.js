56df1537975d14fe59d39bce64ba7a13fc456cbb
Magnet Pro <macbookpro@Magnet-MacBook-Pro.local>
2018-10-22 16:24:13 +0300

    Add a service worker

--- HTML/chef-grup/js/main.js
+++ HTML/chef-grup/js/main.js
@@ -1,4 +1,4 @@
-// Change site language
+/* Change site language */
 var changeToEnglish = document.getElementById('english');
 changeToEnglish.addEventListener('click', function() {
     document.body.className = 'en';
@@ -9,54 +9,14 @@ changeToTurkish.addEventListener('click', function() {
     document.body.className = 'tr';
 });
 
-/* // Navigation
-function displayNoneAllSections() {
-    var sections = document.getElementsByTagName('section');
-    for(var i = 0; i < sections.length; i++)
-        sections[i].style.display = 'none';
+/* Register Service Worker */
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('service_worker.js')
+        .then(function(registration) { 
+            console.log("Service Worker Registered", registration); 
+        })
+        .catch(function(err) {
+            console.log("Service Worker Failed to Register", err);
+        });
 }
-
-function clearActiveStyle() {
-    var activeStyledHeader = document.getElementsByClassName('active');
-    if(activeStyledHeader.length > 0)
-        activeStyledHeader[0].className = activeStyledHeader[0].className.replace(" active", "");
-}
-
-var homeLink = document.getElementById('home-link');
-var homeSection = document.getElementById('home');
-homeLink.addEventListener('click', function(){
-    displayNoneAllSections();
-    homeSection.style.display = "block";
-
-    clearActiveStyle();
-});
-
-var aboutLink = document.getElementById('about-link');
-var aboutSection = document.getElementById('about');
-aboutLink.addEventListener('click', function(){
-    displayNoneAllSections();
-    aboutSection.style.display = "block";
-
-    clearActiveStyle();
-    this.className += " active";
-});
-
-var brandsLink = document.getElementById('brands-link');
-var brandsSection = document.getElementById('brands');
-brandsLink.addEventListener('click', function(){
-    displayNoneAllSections();
-    brandsSection.style.display = "block";
-
-    clearActiveStyle();
-    this.className += " active";
-});
-
-var contactLink = document.getElementById('contact-link');
-var contactSection = document.getElementById('contact');
-contactLink.addEventListener('click', function(){
-    displayNoneAllSections();
-    contactSection.style.display = "block";
-
-    clearActiveStyle();
-    this.className += " active";
-}); */
---
+++ HTML/chef-grup/service_worker.js
@@ -0,0 +1,39 @@
+importScripts('./js/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+    console.log('Attempting to install service worker and cache static assets');
+    e.waitUntil(
+        caches.open('chefgrup').then(function(cache) {
+            return cache.addAll([
+                '',
+                'index.html',
+                'about.html',
+                'brands.html',
+                'contact.html',
+                'images/hero-image-1.jpg',
+                'images/hero-image-2.jpg',
+                'images/hero-image-3.jpg',
+                'images/logo-chef-doner.png',
+                'images/logo-chef-meyhane.png',
+                'images/logo-chef-mezze.png',
+                'images/logo.png',
+                'images/rect.png',
+                'images/team-image.png',
+                'images/team-image-2.JPG',
+                'css/contact.css',
+                'css/main.css',
+                'css/nav.css',
+                'js/main.js'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
