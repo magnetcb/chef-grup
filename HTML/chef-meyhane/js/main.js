f804965cec4826a57d7895a4081727dd3af9afa1
Magnet Pro <macbookpro@Magnet-MacBook-Pro.local>
2018-10-31 09:51:31 +0200

    Add max-width to body & Make back to top button fixe & Add transitions

--- HTML/chef-meyhane/js/main.js
+++ HTML/chef-meyhane/js/main.js
@@ -99,3 +99,21 @@ function toggle (elements, specifiedDisplay) {
     return window.getComputedStyle(element, null).getPropertyValue('display') === 'none';
   }
 }
+
+// BACK TO TOP Button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top").style.display = "block";
+    } else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
