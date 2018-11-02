b4e8843dd37a70e63816897bdd467d1ef00b0590
Magnet Pro <macbookpro@Magnet-MacBook-Pro.local>
2018-11-02 16:49:59 +0200

    Add section contents

--- HTML/chef-doner/js/main.js
+++ HTML/chef-doner/js/main.js
@@ -1,13 +1,15 @@
 var myFullpage = new fullpage('#fullpage', {
 	licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
 
+	//Accessibility
+	keyboardScrolling: true,
+	animateAnchor: true,
+	recordHistory: true,
+
 	//Design
-	controlArrows: true,
 	verticalCentered: true,
 	paddingTop: '3em',
 	paddingBottom: '10px',
-	responsiveWidth: 0,
-	responsiveHeight: 0,
 	responsiveSlides: false,
 	parallax: false,
 	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
