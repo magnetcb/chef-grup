4dc50e974444cd722e70ba111dcbfffb01979cc7
Magnet Pro <macbookpro@Magnet-MacBook-Pro.local>
2018-11-02 15:37:10 +0200

    Project init

---
+++ HTML/chef-doner/js/main.js
@@ -0,0 +1,14 @@
+var myFullpage = new fullpage('#fullpage', {
+	licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+
+	//Design
+	controlArrows: true,
+	verticalCentered: true,
+	paddingTop: '3em',
+	paddingBottom: '10px',
+	responsiveWidth: 0,
+	responsiveHeight: 0,
+	responsiveSlides: false,
+	parallax: false,
+	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+});
