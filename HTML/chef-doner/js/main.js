6f2af0b52a804565993cbfd0b8cd2f16e651639d
Magnet Pro <macbookpro@Magnet-MacBook-Pro.local>
2018-11-05 11:02:36 +0200

    Add CNAME, Add navigation, Responsive main page

--- HTML/chef-doner/js/main.js
+++ HTML/chef-doner/js/main.js
@@ -1,6 +1,16 @@
 var myFullpage = new fullpage('#fullpage', {
 	licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
 
+	//Navigation
+	menu: '#navbar',
+	lockAnchors: false,
+	anchors:['homePage', 'aboutPage', 'menuPage'],
+	navigation: false,
+	navigationPosition: 'left',
+	showActiveTooltip: false,
+	slidesNavigation: false,
+	slidesNavPosition: 'bottom',
+
 	//Accessibility
 	keyboardScrolling: true,
 	animateAnchor: true,
@@ -12,5 +22,5 @@ var myFullpage = new fullpage('#fullpage', {
 	paddingBottom: '10px',
 	responsiveSlides: false,
 	parallax: false,
-	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+	parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'}
 });
