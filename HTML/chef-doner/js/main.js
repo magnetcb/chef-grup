39ad33125cd0ce5933cd877f8b41c94b8d9ebd29
mrsimsek <denizsimsek93@gmail.com>
2018-11-08 11:50:38 +0300

    Add fullPage.js files over CDN, Add slide images, Add menu modal

--- HTML/chef-doner/js/main.js
+++ HTML/chef-doner/js/main.js
@@ -11,6 +11,9 @@ var myFullpage = new fullpage('#fullpage', {
 	slidesNavigation: false,
 	slidesNavPosition: 'bottom',
 
+	//Scrolling
+	scrollOverflow: true,
+
 	//Accessibility
 	keyboardScrolling: true,
 	animateAnchor: true,
@@ -41,4 +44,21 @@ function initMap() {
 		position: uluru, 
 		map: map
 	});
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var menuButton = document.getElementById('menu-button');
+menuButton.onclick = function(){
+    modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
